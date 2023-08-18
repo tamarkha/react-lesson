4cc44787643a0f1d96644e234640cc6ddf1730d7
tamarkha <127031669+tamarkha@users.noreply.github.com>
2023-08-19 03:03:22 +0400

    add social media links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -22,12 +22,12 @@ const Footer = () => {
             <Header> Contact</Header>
             <List>
               <Item>
-                <Link to="#">
+                <Link to="https://twitter.com/i/flow/login?input_flow_data=%7B%22requested_variant%22%3A%22eyJsYW5nIjoiZW4ifQ%3D%3D%22%7D">
                   <FaTwitter></FaTwitter>
                 </Link>
               </Item>
               <Item>
-                <Link to="#">
+                <Link to="https://www.linkedin.com/?original_referer=https%3A%2F%2Fwww.google.com%2F">
                   <FaLinkedin></FaLinkedin>
                 </Link>
               </Item>
